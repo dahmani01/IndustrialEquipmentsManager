494ac6bba0e0a1ba6552d4b215c41214abce1a02
Ahmed <adahmani010@gmail.com>
2022-09-10 18:24:50 +0100

    Update User Dtos

--- CPG_Platform/Dtos/User/UserRegisterDto.cs
+++ CPG_Platform/Dtos/User/UserRegisterDto.cs
@@ -1,8 +1,14 @@
-﻿namespace RPG_Game.Dtos.User
+﻿using CPG_Platform.Models;
+
+namespace RPG_Game.Dtos.User
 {
     public class UserRegisterDto
     {
-        public string Username { get; set; } = string.Empty;
+        public string Matricule { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public Secteur Secteur { get; set; }
     }
 }
